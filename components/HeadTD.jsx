098e1abb108f0455712d8e9a40227842b0dec6c2
geza mahardika <60805115+Gezawahaha@users.noreply.github.com>
2023-10-11 09:01:06 +0000

    Update HeadTD.jsx

--- components/HeadTD.jsx
+++ components/HeadTD.jsx
@@ -10,30 +10,30 @@ const HeadTD = ({ newTitle, newDesc }) => (
         : `Sejuta Cita Book's App`}
     </title>
     <script
-            async
-            defer
-            dangerouslySetInnerHTML={{
-              __html: `(function (m, a, z, e) {
-                var s, t;
-                try {
-                  t = m.sessionStorage.getItem('maze-us');
-                } catch (err) {}
-              
-                if (!t) {
-                  t = new Date().getTime();
-                  try {
-                    m.sessionStorage.setItem('maze-us', t);
-                  } catch (err) {}
-                }
-              
-                s = a.createElement('script');
-                s.src = z + '?t=' + t + '&apiKey=' + e;
-                s.async = true;
-                a.getElementsByTagName('head')[0].appendChild(s);
-                m.mazeUniversalSnippetApiKey = e;
-              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd576d76c-a884-4cee-81fd-2bfd6aa718e7');`,
-            }}
-        />
+      async
+      defer
+      dangerouslySetInnerHTML={{
+        __html: `(function (m, a, z, e) {
+          var s, t;
+          try {
+            t = m.sessionStorage.getItem('maze-us');
+          } catch (err) {}
+        
+          if (!t) {
+            t = new Date().getTime();
+            try {
+              m.sessionStorage.setItem('maze-us', t);
+            } catch (err) {}
+          }
+        
+          s = a.createElement('script');
+          s.src = z + '?t=' + t + '&apiKey=' + e;
+          s.async = true;
+          a.getElementsByTagName('head')[0].appendChild(s);
+          m.mazeUniversalSnippetApiKey = e;
+        })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd576d76c-a884-4cee-81fd-2bfd6aa718e7');`,
+      }}
+    />
     <meta
       name="description"
       content={
