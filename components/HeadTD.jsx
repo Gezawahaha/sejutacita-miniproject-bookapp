9bd51a474a44d3b085c801be0d1d30f38204b82b
geza mahardika <60805115+Gezawahaha@users.noreply.github.com>
2023-10-11 07:20:35 +0000

    Update HeadTD.jsx

--- components/HeadTD.jsx
+++ components/HeadTD.jsx
@@ -3,11 +3,33 @@ import Head from 'next/head';
 
 const HeadTD = ({ newTitle, newDesc }) => (
   <Head>
+    
     <title>
       {newTitle
         ? `${newTitle} | Sejuta Cita Book's App`
         : `Sejuta Cita Book's App`}
     </title>
+    <script>
+      (function (m, a, z, e) {
+        var s, t;
+        try {
+          t = m.sessionStorage.getItem('maze-us');
+        } catch (err) {}
+      
+        if (!t) {
+          t = new Date().getTime();
+          try {
+            m.sessionStorage.setItem('maze-us', t);
+          } catch (err) {}
+        }
+      
+        s = a.createElement('script');
+        s.src = z + '?t=' + t + '&apiKey=' + e;
+        s.async = true;
+        a.getElementsByTagName('head')[0].appendChild(s);
+        m.mazeUniversalSnippetApiKey = e;
+      })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd576d76c-a884-4cee-81fd-2bfd6aa718e7');
+      </script>
     <meta
       name="description"
       content={
@@ -16,6 +38,7 @@ const HeadTD = ({ newTitle, newDesc }) => (
           : `This Official Sejuta Cita Book's App. You can read anything book's just visit this website`
       }
     />
+    
   </Head>
 );
 
