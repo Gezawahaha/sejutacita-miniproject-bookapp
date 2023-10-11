c457b05afba583d5a82f3d05a952d925bb8e7fda
geza mahardika <60805115+Gezawahaha@users.noreply.github.com>
2023-10-11 07:24:49 +0000

    Update SCript HeadTD.jsx

--- components/HeadTD.jsx
+++ components/HeadTD.jsx
@@ -9,27 +9,31 @@ const HeadTD = ({ newTitle, newDesc }) => (
         ? `${newTitle} | Sejuta Cita Book's App`
         : `Sejuta Cita Book's App`}
     </title>
-    <script>
-      (function (m, a, z, e) {
-        var s, t;
-        try {
-          t = m.sessionStorage.getItem('maze-us');
-        } catch (err) {}
-      
-        if (!t) {
-          t = new Date().getTime();
-          try {
-            m.sessionStorage.setItem('maze-us', t);
-          } catch (err) {}
-        }
-      
-        s = a.createElement('script');
-        s.src = z + '?t=' + t + '&apiKey=' + e;
-        s.async = true;
-        a.getElementsByTagName('head')[0].appendChild(s);
-        m.mazeUniversalSnippetApiKey = e;
-      })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd576d76c-a884-4cee-81fd-2bfd6aa718e7');
-      </script>
+    <script
+            async
+            defer
+            dangerouslySetInnerHTML={{
+              __html: `(function (m, a, z, e) {
+                var s, t;
+                try {
+                  t = m.sessionStorage.getItem('maze-us');
+                } catch (err) {}
+              
+                if (!t) {
+                  t = new Date().getTime();
+                  try {
+                    m.sessionStorage.setItem('maze-us', t);
+                  } catch (err) {}
+                }
+              
+                s = a.createElement('script');
+                s.src = z + '?t=' + t + '&apiKey=' + e;
+                s.async = true;
+                a.getElementsByTagName('head')[0].appendChild(s);
+                m.mazeUniversalSnippetApiKey = e;
+              })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'd576d76c-a884-4cee-81fd-2bfd6aa718e7');`,
+            }}
+        />
     <meta
       name="description"
       content={
